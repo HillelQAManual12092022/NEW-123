435a5d30bf70a53542f4c758a7849a337a9cff43
Rostyslav666 <121452797+Rostyslav666@users.noreply.github.com>
2022-12-26 20:40:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script><![endif]-->
+	<title>Perfect 1.3.6</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta name="viewport" content="max-width=device-width, initial-scale=1">
+	<meta name="keywords"
+		content="perfect world, pw, перфект ворлд 136, пв, pw 1.3.6, пв 1.3.6, perfect world 1.3.6, pw 136, пв 136, perfect136, perfect 136, скачать пв, " />
+	<link rel="SHORTCUT ICON" href="images/favicon.png">
+	<link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+	<header>
+		<div class="top-panel">
+			<div class="container flex-s-c">
+				<a href="#" class="logo bright">
+					<img src="images/2-100.png" alt="">
+				</a>
+				<div class="button-btn buttonDrop" data-class="main-menu">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<nav class="main-menu flex">
+					<li class="active"><a href="">Главная</a></li>
+					<li><a href="https://lk.perfect136.com/register.php">Регистрация</a></li>
+					<li><a
+							href="https://perfect136.com/forum/index.php?threads/%D0%9D%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D0%B8%D0%B3%D1%80%D0%B0%D1%82%D1%8C-%D0%BD%D0%B0-perfect136-to-start-playing-on-perfect136.24/">Скачать</a>
+					</li>
+					<li><a href="https://perfect136.com/forum/index.php">Форум</a></li>
+					<li><a
+							href="https://perfect136.com/forum/index.php?threads/%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%BA%D0%B0-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D0%B5%D0%B9-users-support.25/#post-30">Поддержка</a>
+					</li>
+					<a href="https://lk.perfect136.com/" class="btn user-account-btn-mobile">Личный кабинет</a>
+				</nav>
+				<div class="select-lang">
+					<div class="lang-block">
+						<a href="javascript:void(0);" class="main-item"><img class="img-lang" src="images/ru-icon.png"
+								alt=""></a>
+						<ul class="hidden-block">
+							<li><a href="" class="main-item"><img class="img-lang" src="images/ru-icon.png" alt="">
+									Русский </a></li>
+							<li><a href="" class="main-item"><img class="img-lang" src="images/eng-icon.png" alt="">
+									English </a></li>
+						</ul>
+					</div>
+				</div>
+				<a href="https://lk.perfect136.com/" class="btn user-account-btn">Личный кабинет</a>
+			</div>
+		</div>
+		<div class="intro">
+			<div class="container flex">
+				<h1>
+					ТВОЙ <br>
+					<p><span>ИДЕАЛЬНЫЙ МИР</span></p>
+				</h1>
+				<p>
+					Это возможность телепорации во времени,
+					в те времена когда лучшая MMOPRG дарила лишь положительные эмоции!
+
+				</p>
+				<a href="https://perfect136.com/forum/index.php?threads/%D0%9D%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D0%B8%D0%B3%D1%80%D0%B0%D1%82%D1%8C-%D0%BD%D0%B0-perfect136-to-start-playing-on-perfect136.24/"
+					class="btn start-game-btn">Начать игру</a>
+			</div>
+		</div>
+		<div class="periy">
+			<div class="ani stone s1 on"></div>
+			<div class="ani stone s2 on"></div>
+			<div class="ani stone s3 on"></div>
+		</div>
+	</header>
+	<!-- <div class="wrapper"> -->
+	<main class="page">
+		<div class="container">
+			<section class="games flex-s-c">
+				<div class="game flex">
+
+					<div class="game-info">
+						<h3 class="title-game">
+							Рейты <span class="x">x1</span>
+						</h3>
+						<p class="players-in-game">
+							Классическая версия
+						</p>
+
+					</div>
+				</div>
+				<div class="game flex">
+
+					<div class="game-info">
+						<h3 class="title-game">
+							<?php
+                                                     include "dog.php";
+                                                     ?>
+							Игроков в сети: <span class="players-amount">
+								<?php echo floor($umno2) ?>
+							</span>
+						</h3>
+						<div class="accounts">
+
+
+
+						</div>
+						<p class="players-in-game">
+							Статус сервера: <span class="players-amount">ONLINE</span>
+						</p>
+					</div>
+				</div>
+				<div class="game flex">
+
+					<div class="game-info">
+						<h3 class="title-game">
+							Регистраций:
+							<span class="players-amount">
+								<?php echo floor($umno)?>
+							</span>
+						</h3>
+						<p class="players-in-game">
+							Присоединяйтесь к нам!
+						</p>
+
+
+					</div>
+				</div>
+
+			</section>
+			<section class="last-news-n-discord flex-s-c">
+				<div class="last-news">
+
+					<h2 class="title-block">
+						Новости <span></span>
+					</h2>
+					<div class="last-news-block flex-s-c">
+						<div class="main-news">
+							<p class="datetime">01.11.2021 в 18:00</p>
+							<h3 class="news-title">
+								Информация о сервере
+							</h3>
+							<p class="news-content">
+								Идеальная версия игры - 1.3.6, классическая концепция и баланс.
+								Авторские квесты и ивенты, а так же лучшие эмоции от игры!
+							</p>
+							<a href="https://perfect136.com/forum/index.php?threads/%D0%9A%D0%BE%D0%BD%D1%86%D0%B5%D0%BF%D1%86%D0%B8%D1%8F-%D1%81%D0%B5%D1%80%D0%B2%D0%B5%D1%80%D0%B0.1/"
+								class="btn read-more-btn">Читать</a>
+						</div>
+						<div class="side-news-block flex-s-c">
+							<div class="side-news flex">
+								<img src="images/side-news-image-1.jpg" alt="">
+								<div class="side-news-info">
+									<p class="datetime"></p>
+									<h3 class="news-title">
+										Бонусы новичкам
+									</h3>
+									<p class="news-content">
+										Поспеши получить идеальный бонус,
+										а также забрать призы за первые достижения.
+									</p>
+									<a href="https://perfect136.com/forum/index.php?threads/%D0%91%D0%BE%D0%BD%D1%83%D1%81%D1%8B-%D0%BD%D0%BE%D0%B2%D0%B8%D1%87%D0%BA%D0%B0%D0%BC-bonuses-for-beginners.29/"
+										class="btn read-more-btn">Читать</a>
+								</div>
+							</div>
+							<div class="side-news flex">
+								<img src="images/side-news-image-2.jpg" alt="">
+								<div class="side-news-info">
+									<p class="datetime"></p>
+									<h3 class="news-title">
+										Бонусы переходящим кланам
+									</h3>
+									<p class="news-content">
+										Предоставляем кланам реферальную програму
+										при старте, при долгом сотрудничестве
+										и дополнительные бонусы на игровой магазин.
+									</p>
+									<a href="https://perfect136.com/forum/index.php?threads/%D0%91%D0%BE%D0%BD%D1%83%D1%81%D1%8B-%D0%BA%D0%BB%D0%B0%D0%BD%D0%B0%D0%BC-bonuses-for-guilds.9/"
+										class="btn read-more-btn">Читать</a>
+								</div>
+							</div>
+							<div class="side-news flex">
+								<img src="images/side-news-image-3.jpg" alt="">
+								<div class="side-news-info">
+									<p class="datetime"></p>
+									<h3 class="news-title">
+										F.A.Q.
+									</h3>
+									<p class="news-content news-content-faq">
+										Часто задаваемые вопросы
+									</p>
+									<a href="https://perfect136.com/forum/index.php?threads/%D0%A7%D0%B0%D1%81%D1%82%D0%BE-%D0%B7%D0%B0%D0%B4%D0%B0%D0%B2%D0%B0%D0%B5%D0%BC%D1%8B%D0%B5-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B.6/"
+										class="btn read-more-btn">Читать</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<a href="https://perfect136.com/forum/index.php" class="show-more-news">Больше информации на
+						нашем
+						форуме</a>
+				</div>
+				<div class="discord">
+					<h2 class="title-block title-block-table">
+						Топ <span>Игроков</span>
+					</h2>
+					<div class="content">
+						<div class="navi">
+							<table cellspacing="0">
+								<thead>
+									<tr>
+										<th>№</th>
+										<th>Никнейм</th>
+										<th width="">Ур.</th>
+										<th>Класс</th>
+										<th width="">Репутация</th>
+									</tr>
+								</thead>
+								<tbody class="table-content">
+									<?php
+include "configuration.php"; 
+$mc = Mysql_Query("SELECT @i:=@i+1 as num, t.* FROM p_top AS t, (SELECT @i:=0) as table_name ORDER by t.level DESC LIMIT 10");
+while ($top = mysql_fetch_array($mc)) 
+{
+	echo <<<HTML
+
+
+	<tr>
+<td>{$top['num']}</td>
+<td>{$top['name']}</td>
+<td>{$top['level']}</td>
+<td><img src="images/icon/{$top['cls']}.png"></td>
+<td>{$top['reputation']}</td>
+	</tr>
+HTML;
+}
+$mc = mysql_close();
+
+?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</main>
+	<!-- </div> -->
+	<footer>
+		<div class="container flex-s-c">
+			<div class="copy-n-info">
+				<p class="copy">
+					© 2021 PERFECT136.COM
+				</p>
+				<p class="info">
+					Наш сервер является тестовым и создан для ознакомления с игрой «Perfect World» <br>
+					все права принадлежат правообладателю.
+				</p>
+				<div class="rules-links flex">
+					<a href="https://perfect136.com/forum/index.php?threads/%D0%9F%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D0%B0-%D1%81%D0%B5%D1%80%D0%B2%D0%B5%D1%80%D0%B0.5/">Правила проекта</a>
+					<a href="#"></a>
+					<a href=""></a>
+				</div>
+			</div>
+			<div class="footer-menu flex">
+				<nav>
+					<li><a
+							href="#"></a></li>
+					<!-- <li><a href="#"></a></li> -->
+
+
+				</nav>
+				<!-- <nav>
+					<li><a href="#"></a></li>
+
+				</nav> -->
+			</div>
+			<div class="site-links flex-s-c">
+				<ul class="social-networks flex">
+					<li class="ds"><a href="https://t.me/perfect_136"></a></li>
+					<li class="fb"><a href="https://vk.com/perfect136"></a></li>
+					<li class="yt"><a
+							href="https://www.youtube.com/channel/UCAKFAJQ2H0o4kNU7SCrwwWQ?sub_confirmation=1"></a></li>
+				</ul>
+				<span class="separator"></span>
+				<a href="#"><img src="images/mv.png" alt=""></a>
+			</div>
+		</div>
+	</footer>
+
+	<div id="register" class="modal-div flex-c-c">
+		<img src="images/reg.png" alt="">
+		<div class="modalContent">
+			<div class="modal_close"></div>
+			<div class="modalTitle">
+				<h2>Регистрация</h2>
+				<h3>Игровой аккаунт</h3>
+			</div>
+			<form class="modal-form">
+				<input class="form-input" type="text" placeholder="Login">
+				<input class="form-input" type="password" placeholder="Password">
+				<input class="form-input" type="password" placeholder="Repeat Password">
+				<input class="form-input" type="email" placeholder="E-mail">
+				<div class="enter-button flex-s-c">
+					<input class="form-submit" type="submit" value="Create Account">
+					<a class="login-btn" href="#"><span>Log In</span></a>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div id="overlay"></div>
+
+
+	<!-- <div class="content">
+		<div class="navi">
+			<table cellspacing="0">
+				<thead>
+					<tr>
+						<th>№</th>
+						<th>Никнейм</th>
+						<th width="">Ур.</th>
+						<th>Класс</th>
+						<th width="">Репутация</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+	</div> -->
+
+	<!-- <?php
+include "configuration.php"; 
+$mc = Mysql_Query("SELECT @i:=@i+1 as num, t.* FROM p_top AS t, (SELECT @i:=0) as table_name ORDER by t.level DESC LIMIT 10");
+while ($top = mysql_fetch_array($mc)) 
+{
+	echo <<<HTML
+
+
+
+<td>{$top['num']}</td>
+<td>{$top['name']}</td>
+<td>{$top['level']}</td>
+<td><img src="images/icon/{$top['cls']}.png"></td>
+<td>{$top['reputation']}</td>
+
+	</tr>
+HTML;
+}
+$mc = mysql_close();
+
+?> -->
+
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/global.js"></script>
+</body>
+
+<div style="position:absolute; top:1770px; left:1554px;">
+<a href="https://freekassa.ru" target="_blank" rel="noopener noreferrer">
+<img src="https://perfect136.com/images/freekassa.png" title="Прием платежей на сайте">
+</a>
+</div>
+
+<div style="position:absolute; top:1770px; left:1454px;">
+<a href="https://pw.mmotop.ru/servers/35489/votes/new" target="_blank">
+  <img src="https://perfect136.com/images/mmotop.png" border="0" id="mmotopratingimg" alt="Рейтинг серверов mmotop">
+</a>
+</div>
+
+</html>
